2a91bfc04094bcf66d911e0ce3e16d89a78d0df9
lostdou <facundobott@gmail.com>
2025-03-24 02:33:07 -0300

    lol

--- CashierQueueAPI/Controllers/UsuarioController.cs
+++ CashierQueueAPI/Controllers/UsuarioController.cs
@@ -37,5 +37,75 @@ namespace CashierQueueAPI.Controllers
                 return StatusCode(500, "Internal server error: " + ex.Message);
             }
         }
+
+        [HttpGet]
+        [Route("BuscarUsuario")]
+        public IActionResult BuscarUser([FromQuery] string nombre, [FromQuery] string apellido)
+        {
+            try
+            {
+                using (var conexion = new SqlConnection(cadenaSQL))
+                {
+                    conexion.Open();
+                    string query = @"SELECT 
+                                    idUsuario, rol, usuario, activo FROM 
+                                    USUARIO WHERE 
+                                    nombre = @nombre AND apellido = @apellido";
+                    var parameters = new { nombre, apellido };
+                    var usuarios = conexion.Query(query, parameters).ToList();
+                    return Ok(usuarios);
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal server error: " + ex.Message);
+            }
+        }
+
+        [HttpPut]
+        [Route("Activar/{idUsuario}")]
+        public IActionResult Activar(int idUsuario)
+        {
+            try
+            {
+                using (var conexion = new SqlConnection(cadenaSQL))
+                {
+                    conexion.Open();
+                    string query = @"UPDATE USUARIO 
+                                 SET activo = 1
+                                 WHERE idUsuario = @idUsuario";
+                    var parameters = new { idUsuario };
+                    conexion.Execute(query, parameters);
+                    return StatusCode(200, new { code = "200", mensaje = "Usuario editado correctamente" });
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal server error: " + ex.Message);
+            }
+        }
+
+        [HttpPut]
+        [Route("Desactivar/{idUsuario}")]
+        public IActionResult Desactivar(int idUsuario)
+        {
+            try
+            {
+                using (var conexion = new SqlConnection(cadenaSQL))
+                {
+                    conexion.Open();
+                    string query = @"UPDATE USUARIO 
+                                 SET activo = 0
+                                 WHERE idUsuario = @idUsuario";
+                    var parameters = new { idUsuario };
+                    conexion.Execute(query, parameters);
+                    return StatusCode(200, new { code="200", mensaje = "Usuario editado correctamente" });
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal server error: " + ex.Message);
+            }
+        }
     }
  }
